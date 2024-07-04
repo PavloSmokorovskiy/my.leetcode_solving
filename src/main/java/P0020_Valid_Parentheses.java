a3dc64e61b12790b24a3e02e983769a782e1ecff
Pavlo Smokorovskiy <pavvelsm@gmail.com>
2024-07-03 20:42:12 -0400

    Added StackArray and QueueArray

--- src/main/java/P0020_Valid_Parentheses.java
+++ src/main/java/P0020_Valid_Parentheses.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class P0020_Valid_Parentheses {
 
     /*
@@ -25,13 +30,91 @@ Output: false
      */
 
     public static void main(String[] args) {
-        String s1 = "()[]{}";
-        System.out.println(isValid(s1));
+//        String s1 = "()[]{}";
+//        System.out.println(isValid(s1));
+//
+//        String s2 = "(]";
+//        System.out.println(isValid(s2));
+
+//        String s3 = "{[]}";
+//        System.out.println(isValid(s3));
+
+        String s4 = "((";
+        System.out.println(isValid(s4));
     }
 
+    /*
+1. put bracket into the stack
+2. check if provided bracket is closed
+2.1 if no put bracket into the stack
+2.2 if yes check is it pair to provided bracket remove it
+2.3 if it is no pair return false
+    */
+
     static boolean isValid(String s) {
+        final var length = s.length();
+        if (length <= 1) {
+            return false;
+        }
+
+        var stack = new char[length];
+        var top = -1;
+
+        for (var ch : s.toCharArray()){
+            if (ch == '('){
+                stack[++top] = ')';
+            } else if (ch == '{') {
+                stack[++top] = '}';
+            } else if (ch == '[') {
+                stack[++top] = ']';
+            } else {
+                if (top == -1 || stack[top--] != ch){
+                    return false;
+                }
+            }
+        }
+
+        return top == -1;
+    }
 
 
+    static boolean isValidUnoptimized(String s) {
+
+        final var length = s.length();
+        if (length <= 1) {
+            return false;
+        }
+
+        var stack = new char[length];
+        var top = -1;
+
+        for (var i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (isBracketOpened(ch)) {
+                stack[++top] = ch;
+            } else if (top != -1 && isPair(stack[top], ch)) {
+                top--;
+            } else {
+                return false;
+            }
+        }
+
+        return top == -1;
+    }
+
+    private static boolean isBracketOpened(char ch) {
+        char[] validOpenedBrackets = { '(', '{', '[' };
+        for (char c : validOpenedBrackets) {
+            if (ch == c) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    private static boolean isPair(char chOpen, char chClose) {
+        return (chOpen == '(' && chClose == ')') ||
+                (chOpen == '{' && chClose == '}') ||
+                (chOpen == '[' && chClose == ']');
     }
 }
