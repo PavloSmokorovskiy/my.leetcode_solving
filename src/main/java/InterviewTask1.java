04132181a5a2d493fa8a8b79d3e6b4c6c977769d
Pavlo <pavvelsm@gmail.com>
2025-12-09 01:49:31 +0100

    Added interview task

--- src/main/java/InterviewTask1.java
+++ src/main/java/InterviewTask1.java
@@ -1,5 +1,5 @@
-
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class InterviewTask1 {
     /**
@@ -11,6 +11,52 @@ public class InterviewTask1 {
 
     public static void main(String[] args) {
 
+        int[] arr = new int[]{1, 2, -2, 3, 4, 4, -4, -4, -5, 0};
+        countPairs1(arr);
+        System.out.println("--------''--------");
+        countPairs2(arr);
 
+    }
+
+    static void countPairs1(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int pairs = 0;
+
+        for (int i : arr) {
+
+            int opposite = -i;
+            Integer count = map.get(opposite);
+            if (count != null && count > 0) {
+                pairs++;
+                if (count == 1) {
+                    map.remove(opposite);
+                } else {
+                    map.put(opposite, count - 1);
+                }
+            } else {
+                map.put(i, map.getOrDefault(i, 0) + 1);
+            }
+            System.out.println(pairs);
+            System.out.println(map);
+        }
+    }
+
+    static void countPairs2(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int pairs = 0;
+
+        for (int i : arr) {
+
+            int opposite = -i;
+
+            if (map.containsKey(opposite) && map.get(opposite) != 0) {
+                pairs++;
+                map.put(opposite, map.getOrDefault(opposite, 0) - 1);
+            } else {
+                map.put(i, map.getOrDefault(i, 0) + 1);
+            }
+            System.out.println(pairs);
+            System.out.println(map);
+        }
     }
 }
