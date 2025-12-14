b0c33f481913ce0aab6fc860fc90c797858a02d6
Pavlo <pavvelsm@gmail.com>
2025-12-14 14:08:16 +0100

    Added LeetCode task

--- src/main/java/leetcode/P0485_Max_Consecutive_Ones.java
+++ src/main/java/leetcode/P0485_Max_Consecutive_Ones.java
@@ -5,12 +5,12 @@ public class P0485_Max_Consecutive_Ones {
     /**
      * Given a binary array nums, return the maximum number
      * of consecutive 1's in the array.
-     *
+     * <p>
      * Example 1:
      * Input: nums = [1,1,0,1,1,1]
      * Output: 3
      * Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
-     *
+     * <p>
      * Example 2:
      * Input: nums = [1,0,1,1,0,1]
      * Output: 2
@@ -22,7 +22,19 @@ public class P0485_Max_Consecutive_Ones {
     }
 
     int findMaxConsecutiveOnes(int[] nums) {
+        var max = 0;
+        var count = 0;
 
-        return 0;
+        for (var i : nums) {
+            if (i == 1) {
+                count++;
+            } else {
+                if (count > max) max = count;
+                count = 0;
+            }
+        }
+
+        if (count > max) max = count;
+        return max;
     }
 }
