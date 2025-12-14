82da02a0c70aa804c27fffe0f08ec06d8a26488d
Pavlo <pavvelsm@gmail.com>
2025-12-14 11:49:50 +0100

    Added LeetCode task

---
+++ src/main/java/leetcode/P0383_Ransom_Note.java
@@ -0,0 +1,117 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class P0383_Ransom_Note {
+
+    /**
+     * Given two strings ransomNote and magazine,
+     * return true if ransomNote can be constructed by using the letters from magazine
+     * and false otherwise.
+     * <p>
+     * Each letter in magazine can only be used once in ransomNote.
+     * <p>
+     * Example 1:
+     * Input: ransomNote = "a", magazine = "b"
+     * Output: false
+     * <p>
+     * Example 2:
+     * Input: ransomNote = "aa", magazine = "ab"
+     * Output: false
+     * <p>
+     * Example 3:
+     * Input: ransomNote = "aa", magazine = "aab"
+     * Output: true
+     */
+
+    public static void main(String[] args) {
+        String ransomNote = "aa", magazine = "aab";
+        System.out.println(new P0383_Ransom_Note().canConstruct1(ransomNote, magazine));
+    }
+
+    boolean canConstruct1(String ransomNote, String magazine) {
+        Map<Character, Integer> alphabet = new HashMap<>(26);
+        for (Character c : magazine.toCharArray()) alphabet.put(c, alphabet.getOrDefault(c, 0) + 1);
+        for (Character c : ransomNote.toCharArray()) {
+            int v = alphabet.getOrDefault(c, 0);
+            if (v == 0) return false;
+            alphabet.put(c, --v);
+        }
+        return true;
+    }
+
+    boolean canConstruct2(String ransomNote, String magazine) {
+        int[] alphabet = new int[26];
+        int index;
+        for (char c : ransomNote.toCharArray()) {
+            index = magazine.indexOf(c, alphabet[c - 'a']);
+            if (index == -1) return false;
+            alphabet[c - 'a']++;
+        }
+        return true;
+    }
+
+    boolean canConstruct3(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+        for (char c : magazine.toCharArray()) {
+            letters[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (letters[c - 'a']-- < 0) return false;
+        }
+
+        return true;
+    }
+
+    boolean canConstruct4(String ransomNote, String magazine) {
+
+        char[] magArr = magazine.toCharArray();
+
+        for (char c : ransomNote.toCharArray()) {
+            boolean found = false;
+            for (int j = 0; j < magArr.length; j++) {
+                if (c == magArr[j]) {
+                    magArr[j] = 0;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) return false;
+        }
+
+        return true;
+    }
+
+    boolean canConstruct5(String ransomNote, String magazine) {
+
+        Integer[] magArr = new Integer[magazine.length()];
+        for (int i = 0; i < magazine.length(); i++) {
+            int cp = magazine.codePointAt(i);
+            magArr[i] = cp;
+        }
+
+        Integer[] ranArr = new Integer[ransomNote.length()];
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int cp = ransomNote.codePointAt(i);
+            ranArr[i] = cp;
+        }
+
+        for (int i = 0; i < ranArr.length; i++) {
+            for (int j = 0; j < magArr.length; j++) {
+                if (ranArr[i] == magArr[j]) {
+                    ranArr[i] = 0;
+                    magArr[j] = 0;
+                }
+            }
+        }
+
+        for (int i : ranArr) {
+            if (i != 0) return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/leetcode/quest/Q1_Concatenation_of_Array.java
@@ -0,0 +1,42 @@
+package leetcode.quest;
+
+import java.util.Arrays;
+
+public class Q1_Concatenation_of_Array {
+
+    /**
+     * Given an integer array nums of length n, you want to create an array ans
+     * of length 2n where ans[i] == nums[i] and
+     * ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+     * <p>
+     * Specifically, ans is the concatenation of two nums arrays.
+     * <p>
+     * Return the array ans.
+     * <p>
+     * Example 1:
+     * Input: nums = [1,2,1]
+     * Output: [1,2,1,1,2,1]
+     * Explanation: The array ans is formed as follows:
+     * - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+     * - ans = [1,2,1,1,2,1]
+     * <p>
+     * Example 2:
+     * Input: nums = [1,3,2,1]
+     * Output: [1,3,2,1,1,3,2,1]
+     * Explanation: The array ans is formed as follows:
+     * - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+     * - ans = [1,3,2,1,1,3,2,1]
+     */
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1};
+
+        System.out.println(Arrays.toString(new Q1_Concatenation_of_Array().getConcatenation(nums)));
+    }
+
+    int[] getConcatenation(int[] nums) {
+
+
+        return null;
+    }
+}
