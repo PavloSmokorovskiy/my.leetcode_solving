fdb538494cc3a21547636bfc8f93206b016fc1d1
Pavlo <pavvelsm@gmail.com>
2025-12-14 18:55:36 +0100

    Added LeetCode task

--- src/main/java/leetcode/P1441_Build_an_Array_With_Stack_Operations.java
+++ src/main/java/leetcode/P1441_Build_an_Array_With_Stack_Operations.java
@@ -1,22 +1,22 @@
 package leetcode;
 
-import java.util.List;
+import java.util.*;
 
 public class P1441_Build_an_Array_With_Stack_Operations {
 
     /**
      * You are given an integer array target and an integer n.
-     *
+     * <p>
      * You have an empty stack with the two following operations:
-     *
+     * <p>
      * "Push": pushes an integer to the top of the stack.
      * "Pop": removes the integer on the top of the stack.
      * You also have a stream of the integers in the range [1, n].
-     *
+     * <p>
      * Use the two stack operations to make the numbers in the stack
      * (from the bottom to the top) equal to target. You should follow the
      * following rules:
-     *
+     * <p>
      * If the stream of the integers is not empty, pick the next integer from
      * the stream and push it to the top of the stack.
      * If the stack is not empty, pop the integer at the top of the stack.
@@ -25,7 +25,7 @@ public class P1441_Build_an_Array_With_Stack_Operations {
      * operations on the stack.
      * Return the stack operations needed to build target following the mentioned rules.
      * If there are multiple valid answers, return any of them.
-     *
+     * <p>
      * Example 1:
      * Input: target = [1,3], n = 3
      * Output: ["Push","Push","Pop","Push"]
@@ -34,7 +34,7 @@ public class P1441_Build_an_Array_With_Stack_Operations {
      * Read 2 from the stream and push it to the stack. s = [1,2].
      * Pop the integer on the top of the stack. s = [1].
      * Read 3 from the stream and push it to the stack. s = [1,3].
-     *
+     * <p>
      * Example 2:
      * Input: target = [1,2,3], n = 3
      * Output: ["Push","Push","Push"]
@@ -42,7 +42,7 @@ public class P1441_Build_an_Array_With_Stack_Operations {
      * Read 1 from the stream and push it to the stack. s = [1].
      * Read 2 from the stream and push it to the stack. s = [1,2].
      * Read 3 from the stream and push it to the stack. s = [1,2,3].
-     *
+     * <p>
      * Example 3:
      * Input: target = [1,2], n = 4
      * Output: ["Push","Push"]
@@ -62,8 +62,20 @@ public class P1441_Build_an_Array_With_Stack_Operations {
 
     List<String> buildArray(int[] target, int n) {
 
+        var res = new LinkedList<String>();
+        var stack = new LinkedList<Integer>();
+        var index = 0;
 
+        for (int i = 1; i <= n && index < target.length; i++) {
+            stack.add(i);
+            res.add("Push");
+            if(target[index] == i)
+                index++;
+            else {
+                res.add("Pop");
+            }
+        }
 
-        return null;
+        return res;
     }
 }
