0b81f25c41bf56ca1c6dc60988256702a2133d11
Pavlo <pavvelsm@gmail.com>
2025-12-14 17:17:54 +0100

    Added LeetCode task

--- src/main/java/leetcode/P0448_Find_All_Numbers_Disappeared_in_an_Array.java
+++ src/main/java/leetcode/P0448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class P0448_Find_All_Numbers_Disappeared_in_an_Array {
@@ -8,11 +10,11 @@ public class P0448_Find_All_Numbers_Disappeared_in_an_Array {
      * Given an array nums of n integers where nums[i] is in the range [1, n],
      * return an array of all the integers in the range [1, n]
      * that do not appear in nums.
-     *
+     * <p>
      * Example 1:
      * Input: nums = [4,3,2,7,8,2,3,1]
      * Output: [5,6]
-     *
+     * <p>
      * Example 2:
      * Input: nums = [1,1]
      * Output: [2]
@@ -24,8 +26,16 @@ public class P0448_Find_All_Numbers_Disappeared_in_an_Array {
     }
 
     List<Integer> findDisappearedNumbers(int[] nums) {
+        var res = new ArrayList<Integer>();
+        var seen = new boolean[nums.length + 1];
 
+        for (int num : nums)
+            seen[num] = true;
 
-        return null;
+        for (int i = 1; i < seen.length; i++)
+            if (!seen[i])
+                res.add(i);
+
+        return res;
     }
 }
