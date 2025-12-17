1007940a161efc0b27a87d12729124dc4e0c6168
Pavlo <pavvelsm@gmail.com>
2025-12-17 21:41:22 +0100

    Added LeetCode task

--- src/main/java/leetcode/P1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
+++ src/main/java/leetcode/P1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -18,9 +18,12 @@ public class P1475_Final_Prices_With_a_Special_Discount_in_a_Shop {
      * Input: prices = [8,4,6,2,3]
      * Output: [4,2,4,2,3]
      * Explanation:
-     * For item 0 with price[0]=8 you will receive a discount equivalent to prices[1]=4, therefore, the final price you will pay is 8 - 4 = 4.
-     * For item 1 with price[1]=4 you will receive a discount equivalent to prices[3]=2, therefore, the final price you will pay is 4 - 2 = 2.
-     * For item 2 with price[2]=6 you will receive a discount equivalent to prices[3]=2, therefore, the final price you will pay is 6 - 2 = 4.
+     * For item 0 with price[0]=8 you will receive a discount equivalent to prices[1]=4, therefore,
+     * the final price you will pay is 8 - 4 = 4.
+     * For item 1 with price[1]=4 you will receive a discount equivalent to prices[3]=2, therefore,
+     * the final price you will pay is 4 - 2 = 2.
+     * For item 2 with price[2]=6 you will receive a discount equivalent to prices[3]=2, therefore,
+     * the final price you will pay is 6 - 2 = 4.
      * For items 3 and 4 you will not receive any discount at all.
      * <p>
      * Example 2:
@@ -41,6 +44,15 @@ public class P1475_Final_Prices_With_a_Special_Discount_in_a_Shop {
     int[] finalPrices(int[] prices) {
         var res = new int[prices.length];
 
+        for (int i = 0; i < prices.length; i++) {
+            res[i] = prices[i];
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    res[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
 
         return res;
     }
