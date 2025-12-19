35d345272f039ad53cc62402db7ad18f676c7c89
Pavlo <pavvelsm@gmail.com>
2025-12-19 21:15:25 +0100

    Added LeetCode task

--- src/main/java/leetcode/P0084_Largest_Rectangle_in_Histogram.java
+++ src/main/java/leetcode/P0084_Largest_Rectangle_in_Histogram.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.ArrayDeque;
+
 public class P0084_Largest_Rectangle_in_Histogram {
 
     /**
@@ -18,14 +20,62 @@ public class P0084_Largest_Rectangle_in_Histogram {
      */
 
     public static void main(String[] args) {
-        int[] heights = {2, 1, 5, 6, 2, 3};
-        System.out.println(new P0084_Largest_Rectangle_in_Histogram().largestRectangleArea(heights));
+        int[] heights = {2, 0, 5, 6, 2, 3};
+        System.out.println(new P0084_Largest_Rectangle_in_Histogram().largestRectangleArea1(heights));
     }
 
-    int largestRectangleArea(int[] heights) {
+    int largestRectangleArea1(int[] heights) {
+        var stack = new ArrayDeque<Integer>();
+        var n = heights.length;
+        var maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                var h = heights[stack.pop()];
+                var leftLess = stack.isEmpty() ? -1 : stack.peek();
+                var width = i - leftLess - 1;
+                maxArea = Math.max(maxArea, h * width);
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            var h = heights[stack.pop()];
+            var leftLess = stack.isEmpty() ? -1 : stack.peek();
+            var width = n - leftLess - 1;
+            maxArea = Math.max(maxArea, h * width);
+        }
+
+        return maxArea;
+    }
+
+    int largestRectangleArea2(int[] heights) {
         var res = 0;
+        var n = heights.length;
+        var stack = new ArrayDeque<int[]>();
 
+        for (int i = 0; i < n; i++) {
+            var start = i;
+            var h = heights[i];
 
+            while (!stack.isEmpty() && stack.peek()[1] > h) {
+                var top = stack.pop();
+                var topStart = top[0];
+                var topH = top[1];
+                int area = topH * (i - topStart);
+                res = Math.max(res, area);
+                start = topStart;
+            }
+            stack.push(new int[]{start, heights[i]});
+        }
+
+        while (!stack.isEmpty()) {
+            var top = stack.pop();
+            var topStart = top[0];
+            var topH = top[1];
+            var area = topH * (n - topStart);
+            res = Math.max(res, area);
+        }
 
         return res;
     }
