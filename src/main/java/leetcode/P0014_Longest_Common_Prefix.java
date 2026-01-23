e3e46a9e7fd1b570af9a74a1e44ada4e24762434
Pavlo <pavvelsm@gmail.com>
2026-01-23 08:31:05 +0100

    Added LeetCode tasks

--- src/main/java/leetcode/P0014_Longest_Common_Prefix.java
+++ src/main/java/leetcode/P0014_Longest_Common_Prefix.java
@@ -21,11 +21,41 @@ public class P0014_Longest_Common_Prefix {
 
     public static void main(String[] args) {
         var strs = new String[]{"flower", "flow", "flight"};
-        System.out.println(new P0014_Longest_Common_Prefix().longestCommonPrefix(strs));
+        System.out.println(new P0014_Longest_Common_Prefix().longestCommonPrefixHorisontalScan(strs));
     }
 
-    private String longestCommonPrefix(String[] strs) {
+    private String longestCommonPrefixHorisontalScan(String[] strs) {
 
-        return "";
+        if (strs.length == 0)
+            return "";
+        if (strs.length == 1)
+            return strs[0];
+
+        String prefix = strs[0];
+
+        for (String str : strs) {
+            while (!str.startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+
+    private String longestCommonPrefixVerticalScan(String[] strs) {
+
+        if (strs.length == 0)
+            return "";
+        if (strs.length == 1)
+            return strs[0];
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char ch0 = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++)
+                if (i >= strs[j].length() || ch0 != strs[j].charAt(i))
+                    return strs[0].substring(0, i);
+        }
+        return strs[0];
     }
 }
