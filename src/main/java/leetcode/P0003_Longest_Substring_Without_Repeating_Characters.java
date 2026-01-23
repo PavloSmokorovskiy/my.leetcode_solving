589cf1c153dc57f4361fb83940e6f0a38bee86ff
Pavlo <pavvelsm@gmail.com>
2026-01-23 09:06:22 +0100

    Added LeetCode tasks

--- src/main/java/leetcode/P0003_Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/leetcode/P0003_Longest_Substring_Without_Repeating_Characters.java
@@ -1,34 +1,57 @@
 package leetcode;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class P0003_Longest_Substring_Without_Repeating_Characters {
 
-    /*
-    3. Longest Substring Without Repeating Characters https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
-Given a string s, find the length of the longest
-substring
- without repeating characters.
-
-Example 1:
-Input: s = "abcabcbb"
-Output: 3
-Explanation: The answer is "abc", with the length of 3.
-
-Example 2:
-Input: s = "bbbbb"
-Output: 1
-Explanation: The answer is "b", with the length of 1.
-
-Example 3:
-Input: s = "pwwkew"
-Output: 3
-Explanation: The answer is "wke", with the length of 3.
-Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+    /**
+     * 3. Longest Substring Without Repeating Characters
+     * <p>
+     * Given a string s, find the length of the longest substring without duplicate characters.
+     * <p>
+     * Example 1:
+     * Input: s = "abcabcbb"
+     * Output: 3
+     * Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+     * <p>
+     * Example 2:
+     * Input: s = "bbbbb"
+     * Output: 1
+     * Explanation: The answer is "b", with the length of 1.
+     * <p>
+     * Example 3:
+     * Input: s = "pwwkew"
+     * Output: 3
+     * Explanation: The answer is "wke", with the length of 3.
+     * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
      */
 
-    int lengthOfLongestSubstring(String s) {
+    public static void main(String[] args) {
+        var s = "abcabcbb";
+        System.out.println(new P0003_Longest_Substring_Without_Repeating_Characters().lengthOfLongestSubstringJump(s));
+    }
+
+    private int lengthOfLongestSubstringJump(String s) {
+        Map<Character, Integer> lastIndex = new HashMap<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char current = s.charAt(right);
+
+            if (lastIndex.containsKey(current) && lastIndex.get(current) >= left) left = lastIndex.get(current) + 1;
+
+            lastIndex.put(current, right);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    private int lengthOfLongestSubstring(String s) {
         int n = s.length();
         int maxLength = 0;
         Set<Character> charSet = new HashSet<>();
