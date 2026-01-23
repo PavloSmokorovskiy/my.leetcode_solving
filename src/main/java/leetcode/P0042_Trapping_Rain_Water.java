a30d54b36df5f512d12273e2c6e157855f1eb7c2
Pavlo <pavvelsm@gmail.com>
2026-01-23 18:00:22 +0100

    Added LeetCode tasks

--- src/main/java/leetcode/P0042_Trapping_Rain_Water.java
+++ src/main/java/leetcode/P0042_Trapping_Rain_Water.java
@@ -22,7 +22,24 @@ public class P0042_Trapping_Rain_Water {
     }
 
     private int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int result = 0;
 
-        return 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+
+            if (leftMax <= rightMax) {
+                result += leftMax - height[left];
+                left++;
+            } else {
+                result += rightMax - height[right];
+                right--;
+            }
+        }
+        return result;
     }
 }
