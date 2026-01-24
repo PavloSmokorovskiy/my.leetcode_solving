f1583b1d83f14c0a14301d0090073d85d8514cdf
Pavlo <pavvelsm@gmail.com>
2026-01-24 15:01:40 +0100

    Added LeetCode tasks

--- src/main/java/leetcode/P0459_Repeated_Substring_Pattern.java
+++ src/main/java/leetcode/P0459_Repeated_Substring_Pattern.java
@@ -6,16 +6,16 @@ public class P0459_Repeated_Substring_Pattern {
      * 459. Repeated Substring Pattern
      * Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of
      * the substring together.
-     *
+     * <p>
      * Example 1:
      * Input: s = "abab"
      * Output: true
      * Explanation: It is the substring "ab" twice.
-     *
+     * <p>
      * Example 2:
      * Input: s = "aba"
      * Output: false
-     *
+     * <p>
      * Example 3:
      * Input: s = "abcabcabcabc"
      * Output: true
@@ -27,8 +27,34 @@ public class P0459_Repeated_Substring_Pattern {
         System.out.println(new P0459_Repeated_Substring_Pattern().repeatedSubstringPattern(s));
     }
 
+    /**
+     * - u = s + s
+     * - remove the first and the last symbols
+     * - if u.contains(s) -> true
+     * <p>
+     * Explanation
+     * Positive:
+     * "abсabс" + "abсabс"
+     * "[abс][abс]"
+     * "[abс][abс],[abс][abс]"
+     * "bc][abс][abс][ab"
+     * ^^^  ^^^
+     * abc][abc -> true
+     * <p>
+     * "bc][abс][abс][ab"
+     * <p>
+     * Negative:
+     * "[abс][abd]"
+     * "[abс][abd],[abс][abd]"
+     * "bc][abd][abс][ab"
+     * ^^^  ^^^
+     * abc][abd -> false
+     */
+
     private boolean repeatedSubstringPattern(String s) {
 
-        return false;
+        String doubled = s + s;
+        String middle = doubled.substring(1, doubled.length() - 1);
+        return middle.contains(s);
     }
 }
